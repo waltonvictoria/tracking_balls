e4c1022309b384e29301554515df375ecbcc7220
waltonvictoria <42918286+waltonvictoria@users.noreply.github.com>
2018-10-12 09:14:53 +0700

    chnages

---
+++ index.js
@@ -0,0 +1,36 @@
+function ball1(){
+    document.getElementById("ball1").style.display = "none";
+}
+function ball2(){
+    document.getElementById("ball2").style.display = "none";
+}
+function ball3(){
+    document.getElementById("ball3").style.display = "none";
+}
+function ball4(){
+    document.getElementById("ball4").style.display = "none";
+}
+function ball5(){
+    document.getElementById("ball5").style.display = "none";
+}
+function ball6(){
+    document.getElementById("ball6").style.display = "none";
+}
+function ball7(){
+    document.getElementById("ball7").style.display = "none";
+}
+function ball8(){
+    document.getElementById("ball8").style.display = "none";
+}
+function ball10(){
+    document.getElementById("ball10").style.display = "none";
+}
+function ball20(){
+    document.getElementById("ball20").style.display = "none";
+}
+function ball30(){
+    document.getElementById("ball30").style.display = "none";
+}
+function ball40(){
+    document.getElementById("ball40").style.display = "none";
+}
